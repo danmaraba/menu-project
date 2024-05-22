c9fc8d2c28fa614f617c7b09296e75d67e3d7d39
danmaraba <danielkmaraba@gmail.com>
2024-05-22 14:09:00 +0300

    created  an array of objects

---
+++ app.js
@@ -0,0 +1,50 @@
+const menu=[
+    {
+        Breakfast:{
+            id:1,
+            mealName:"Beverage",
+            image:"./images/beverage.jpg",
+            description:"Lorem ipsum dolor sit amet."
+        }
+    },
+    {
+        Breakfast:{
+            id:2,
+            mealName:"French Toast",
+            image:"./images/french toast.jpg",
+            description:"Lorem ipsum dolor sit amet."
+        }
+    },
+    {
+        Lunch:{
+            id:3,
+            mealName:"Beef Curry",
+            image:"./images/beef curry.jpg",
+            description:"Lorem ipsum dolor sit amet."
+        }
+    },
+    {
+        Lunch:{
+            id:4,
+            mealName:"Fried Rice",
+            image:"./images/friedrice.jpg",
+            description:"Lorem ipsum dolor sit amet."
+        }
+    },
+    {
+        Dinner:{
+            id:5,
+            mealName:"Meat Ball",
+            image:"./images/meatball.jpg",
+            description:"Lorem ipsum dolor sit amet."
+        }
+    },
+    {
+        Dinner:{
+            id:6,
+            mealName:"Salmon",
+            image:"./images/salmon.jpg",
+            description:"Lorem ipsum dolor sit amet."
+        }
+    },
+]
